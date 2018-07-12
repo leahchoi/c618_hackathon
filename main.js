6afa8f715b479dc3049b6b0563e1ad516f69d70b
firecoded <yepyepguru@gmail.com>
2018-07-12 11:34:44 -0700

    changed both red and black pice movement to work better, added king movements as well

--- main.js
+++ main.js
@@ -63,6 +63,8 @@ function clickHandler(){
 	$(".gameboard").on('click', '.black-checker', highlightBlack);
 	$(".gameboard").on('click', '.red-checker', highlightRed);
   	$(".gameboard").on("click", ".highlight", handleCheckerMove);
+  	$(".gameboard").on("click", "redking", highlightRedKing);
+  	$(".gameboard").on("click", "blackking", highlightBlackKing);
 }
 
 function highlightBlack(){
@@ -80,21 +82,27 @@ function highlightBlack(){
 		var twoMovesRowRight = initialRow-2;
 		var twoMovesColRight = initialCol+2;
 
-		if(checkerBoardArray[oneMoveRowLeft][oneMoveColLeft] === ' '){ // if left space is open
-			$(`.play-checker-tile[row=${oneMoveRowLeft}][col=${oneMoveColLeft}]`).addClass('highlight');
-		} else if(checkerBoardArray[oneMoveRowLeft][oneMoveColLeft] === 'r' && checkerBoardArray[twoMovesRowLeft][twoMovesColLeft] === ' '){ // if left is enemy and behind is open
-			$(`.play-checker-tile[row=${twoMovesRowLeft}][col=${twoMovesColLeft}]`).addClass('highlight');
-		} else{ }
-		if(checkerBoardArray[oneMoveRowRight][oneMoveColRight] === ' '){ // if right space is open
-			$(`.play-checker-tile[row=${oneMoveRowRight}][col=${oneMoveColRight}]`).addClass('highlight');
-		} else if(checkerBoardArray[twoMovesRowRight][twoMovesColRight] === undefined){
-			return;
-		} else if(checkerBoardArray[oneMoveRowRight][oneMoveColRight] === 'r' && checkerBoardArray[twoMovesRowRight][twoMovesColRight] === ' '){ // if right is enemy and behind is open
-			$(`.play-checker-tile[row=${twoMovesRowRight}][col=${twoMovesColRight}]`).addClass('highlight');
-		} else{ }
-	} else{
+		if(0 <= oneMoveRowLeft && 0 <= oneMoveColLeft){	
+			if(checkerBoardArray[oneMoveRowLeft][oneMoveColLeft] === ' '){
+				$(`.play-checker-tile[row=${oneMoveRowLeft}][col=${oneMoveColLeft}]`).addClass('highlight');
+			} else if(checkerBoardArray[oneMoveRowLeft][oneMoveColLeft] === 'r' && 0 <= twoMovesRowLeft && 0 <= twoMovesColLeft){
+				if(checkerBoardArray[twoMovesRowLeft][twoMovesColLeft] === ' '){	
+					$(`.play-checker-tile[row=${twoMovesRowLeft}][col=${twoMovesColLeft}]`).addClass('highlight');
+				}	
+			}
+		}	 
+		if(checkerBoardArray.length > oneMoveRowRight && checkerBoardArray.length > oneMoveColRight){	
+			if(checkerBoardArray[oneMoveRowRight][oneMoveColRight] === ' '){
+				$(`.play-checker-tile[row=${oneMoveRowRight}][col=${oneMoveColRight}]`).addClass('highlight');
+			} else if(checkerBoardArray[oneMoveRowRight][oneMoveColRight] === 'r' && 0 <= twoMovesRowRight && 0 <= twoMovesColRight){
+				if(checkerBoardArray[twoMovesRowRight][twoMovesColRight] === ' '){
+					$(`.play-checker-tile[row=${twoMovesRowRight}][col=${twoMovesColRight}]`).addClass('highlight');
+				}
+			}
+		}	 
+	} else {
 		return;
-	}
+	}	
 }
 function highlightRed(){
 	if (redTurn){
@@ -110,25 +118,158 @@ function highlightRed(){
 		var twoMovesRowRight = initialRow+2;
 		var twoMovesColRight = initialCol+2;
 
-		if(checkerBoardArray[(oneMoveRowLeft)][(oneMoveColLeft)] === ' '){
-			$(`.play-checker-tile[row=${oneMoveRowLeft}][col=${oneMoveColLeft}]`).addClass('highlight');
-		} else if(checkerBoardArray[oneMoveRowLeft][oneMoveColLeft] === 'b' && checkerBoardArray[twoMovesRowLeft][twoMovesColLeft] === ' '){
-			$(`.play-checker-tile[row=${twoMovesRowLeft}][col=${twoMovesColLeft}]`).addClass('highlight');
-		} else{ }
-		if(checkerBoardArray[(oneMoveRowRight)][(oneMoveColRight)] === ' '){
-			$(`.play-checker-tile[row=${oneMoveRowRight}][col=${oneMoveColRight}]`).addClass('highlight');
-		} else if(checkerBoardArray[twoMovesRowRight][twoMovesColRight] === undefined){
-			return;
-		} else if(checkerBoardArray[oneMoveRowRight][oneMoveColRight] === 'b' && checkerBoardArray[twoMovesRowRight][twoMovesColRight] === ' '){
-			$(`.play-checker-tile[row=${twoMovesRowRight}][col=${twoMovesColRight}]`).addClass('highlight');
-		} 
+		if(checkerBoardArray.length > oneMoveRowLeft && checkerBoardArray.length > oneMoveColLeft){	
+			if(checkerBoardArray[oneMoveRowLeft][oneMoveColLeft] === ' '){
+				$(`.play-checker-tile[row=${oneMoveRowLeft}][col=${oneMoveColLeft}]`).addClass('highlight');
+			} else if(checkerBoardArray[oneMoveRowLeft][oneMoveColLeft] === 'b' && checkerBoardArray.length > twoMovesRowLeft && checkerBoardArray.length > twoMovesColLeft){
+				if(checkerBoardArray[twoMovesRowLeft][twoMovesColLeft] === ' '){	
+					$(`.play-checker-tile[row=${twoMovesRowLeft}][col=${twoMovesColLeft}]`).addClass('highlight');
+				}	
+			}
+		}	 
+		if(checkerBoardArray.length > oneMoveRowRight && checkerBoardArray.length > oneMoveColRight){	
+			if(checkerBoardArray[oneMoveRowRight][oneMoveColRight] === ' '){
+				$(`.play-checker-tile[row=${oneMoveRowRight}][col=${oneMoveColRight}]`).addClass('highlight');
+			} else if(checkerBoardArray[oneMoveRowRight][oneMoveColRight] === 'b' && checkerBoardArray.length > twoMovesRowRight && checkerBoardArray.length > twoMovesColRight){
+				if(checkerBoardArray[twoMovesRowRight][twoMovesColRight] === ' '){
+					$(`.play-checker-tile[row=${twoMovesRowRight}][col=${twoMovesColRight}]`).addClass('highlight');
+				}
+			}
+		}	 
 	} else {
 		return;
 	}	
 }
 
+function highlightRedKing(){
+	if (redTurn){
+			$('.play-checker-tile').removeClass('highlight');
+			initialRow = parseInt($(this).attr('row'));
+			initialCol = parseInt($(this).attr('col'));
+		var oneMoveRowLeftDown = initialRow+1;
+		var oneMoveColLeftDown = initialCol-1;
+		var oneMoveRowRightDown = initialRow+1;
+		var oneMoveColRightDown = initialCol+1;
+		var twoMovesRowLeftDown = initialRow+2;
+		var twoMovesColLeftDown = initialCol-2;
+		var twoMovesRowRightDown = initialRow+2;
+		var twoMovesColRightDown = initialCol+2;
+		var oneMoveRowLeftUp = initialRow-1;
+		var oneMoveColLeftUp = initialCol-1;
+		var oneMoveRowRightUp = initialRow-1;
+		var oneMoveColRightUp = initialCol+1;
+		var twoMovesRowLeftUp = initialRow-2;
+		var twoMovesColLeftUp = initialCol-2;
+		var twoMovesRowRightUp = initialRow-2;
+		var twoMovesColRight = initialCol+2;
+
+		if(checkerBoardArray.length > oneMoveRowLeftDown && checkerBoardArray.length > oneMoveColLeftDown){	
+			if(checkerBoardArray[oneMoveRowLeftDown][oneMoveColLeftDown] === ' '){
+				$(`.play-checker-tile[row=${oneMoveRowLeftDown}][col=${oneMoveColLeftDown}]`).addClass('highlight');
+			} else if(checkerBoardArray[oneMoveRowLeftDown][oneMoveColLeftDown] === 'b' && checkerBoardArray.length > twoMovesRowLeftDown && checkerBoardArray.length > twoMovesColLeftDown){
+				if(checkerBoardArray[twoMovesRowLeftDown][twoMovesColLeftDown] === ' '){	
+					$(`.play-checker-tile[row=${twoMovesRowLeftDown}][col=${twoMovesColLeftDown}]`).addClass('highlight');
+				}	
+			}
+		}	 
+		if(checkerBoardArray.length > oneMoveRowRightDown && checkerBoardArray.length > oneMoveColRightDown){	
+			if(checkerBoardArray[oneMoveRowRightDown][oneMoveColRightDown] === ' '){
+				$(`.play-checker-tile[row=${oneMoveRowRightDown}][col=${oneMoveColRightDown}]`).addClass('highlight');
+			} else if(checkerBoardArray[oneMoveRowRightDown][oneMoveColRightDown] === 'b' && checkerBoardArray.length > twoMovesRowRightDown && checkerBoardArray.length > twoMovesColRightDown){
+				if(checkerBoardArray[twoMovesRowRightDown][twoMovesColRightDown] === ' '){
+					$(`.play-checker-tile[row=${twoMovesRowRightDown}][col=${twoMovesColRightDown}]`).addClass('highlight');
+				}
+			}
+		}
+		if(0 <= oneMoveRowLeftUp && 0 <= oneMoveColLeftUp){	
+			if(checkerBoardArray[oneMoveRowLeftUp][oneMoveColLeftUp] === ' '){
+				$(`.play-checker-tile[row=${oneMoveRowLeftUp}][col=${oneMoveColLeftUp}]`).addClass('highlight');
+			} else if(checkerBoardArray[oneMoveRowLeftUp][oneMoveColLeftUp] === 'b' && 0 <= twoMovesRowLeftUp && 0 <= twoMovesColLeftUp){
+				if(checkerBoardArray[twoMovesRowLeftUp][twoMovesColLeftUp] === ' '){	
+					$(`.play-checker-tile[row=${twoMovesRowLeftUp}][col=${twoMovesColLeftUp}]`).addClass('highlight');
+				}	
+			}
+		}	 
+		if(checkerBoardArray.length > oneMoveRowRightUp && checkerBoardArray.length > oneMoveColRightUp){	
+			if(checkerBoardArray[oneMoveRowRightUp][oneMoveColRightUp] === ' '){
+				$(`.play-checker-tile[row=${oneMoveRowRightUp}][col=${oneMoveColRightUp}]`).addClass('highlight');
+			} else if(checkerBoardArray[oneMoveRowRightUp][oneMoveColRightUp] === 'b' && 0 <= twoMovesRowRightUp && 0 <= twoMovesColRight){
+				if(checkerBoardArray[twoMovesRowRightUp][twoMovesColRight] === ' '){
+					$(`.play-checker-tile[row=${twoMovesRowRightUp}][col=${twoMovesColRight}]`).addClass('highlight');
+				}
+			}
+		}		 
+	} else {
+		return;
+	}	
+}
+function highlightBlackKing(){
+	if (blackTurn){
+			$('.play-checker-tile').removeClass('highlight');
+			initialRow = parseInt($(this).attr('row'));
+			initialCol = parseInt($(this).attr('col'));
+		var oneMoveRowLeftDown = initialRow+1;
+		var oneMoveColLeftDown = initialCol-1;
+		var oneMoveRowRightDown = initialRow+1;
+		var oneMoveColRightDown = initialCol+1;
+		var twoMovesRowLeftDown = initialRow+2;
+		var twoMovesColLeftDown = initialCol-2;
+		var twoMovesRowRightDown = initialRow+2;
+		var twoMovesColRightDown = initialCol+2;
+		var oneMoveRowLeftUp = initialRow-1;
+		var oneMoveColLeftUp = initialCol-1;
+		var oneMoveRowRightUp = initialRow-1;
+		var oneMoveColRightUp = initialCol+1;
+		var twoMovesRowLeftUp = initialRow-2;
+		var twoMovesColLeftUp = initialCol-2;
+		var twoMovesRowRightUp = initialRow-2;
+		var twoMovesColRight = initialCol+2;
+
+		if(checkerBoardArray.length > oneMoveRowLeftDown && checkerBoardArray.length > oneMoveColLeftDown){	
+			if(checkerBoardArray[oneMoveRowLeftDown][oneMoveColLeftDown] === ' '){
+				$(`.play-checker-tile[row=${oneMoveRowLeftDown}][col=${oneMoveColLeftDown}]`).addClass('highlight');
+			} else if(checkerBoardArray[oneMoveRowLeftDown][oneMoveColLeftDown] === 'r' && checkerBoardArray.length > twoMovesRowLeftDown && checkerBoardArray.length > twoMovesColLeftDown){
+				if(checkerBoardArray[twoMovesRowLeftDown][twoMovesColLeftDown] === ' '){	
+					$(`.play-checker-tile[row=${twoMovesRowLeftDown}][col=${twoMovesColLeftDown}]`).addClass('highlight');
+				}	
+			}
+		}	 
+		if(checkerBoardArray.length > oneMoveRowRightDown && checkerBoardArray.length > oneMoveColRightDown){	
+			if(checkerBoardArray[oneMoveRowRightDown][oneMoveColRightDown] === ' '){
+				$(`.play-checker-tile[row=${oneMoveRowRightDown}][col=${oneMoveColRightDown}]`).addClass('highlight');
+			} else if(checkerBoardArray[oneMoveRowRightDown][oneMoveColRightDown] === 'r' && checkerBoardArray.length > twoMovesRowRightDown && checkerBoardArray.length > twoMovesColRightDown){
+				if(checkerBoardArray[twoMovesRowRightDown][twoMovesColRightDown] === ' '){
+					$(`.play-checker-tile[row=${twoMovesRowRightDown}][col=${twoMovesColRightDown}]`).addClass('highlight');
+				}
+			}
+		}
+		if(0 <= oneMoveRowLeftUp && 0 <= oneMoveColLeftUp){	
+			if(checkerBoardArray[oneMoveRowLeftUp][oneMoveColLeftUp] === ' '){
+				$(`.play-checker-tile[row=${oneMoveRowLeftUp}][col=${oneMoveColLeftUp}]`).addClass('highlight');
+			} else if(checkerBoardArray[oneMoveRowLeftUp][oneMoveColLeftUp] === 'r' && 0 <= twoMovesRowLeftUp && 0 <= twoMovesColLeftUp){
+				if(checkerBoardArray[twoMovesRowLeftUp][twoMovesColLeftUp] === ' '){	
+					$(`.play-checker-tile[row=${twoMovesRowLeftUp}][col=${twoMovesColLeftUp}]`).addClass('highlight');
+				}	
+			}
+		}	 
+		if(checkerBoardArray.length > oneMoveRowRightUp && checkerBoardArray.length > oneMoveColRightUp){	
+			if(checkerBoardArray[oneMoveRowRightUp][oneMoveColRightUp] === ' '){
+				$(`.play-checker-tile[row=${oneMoveRowRightUp}][col=${oneMoveColRightUp}]`).addClass('highlight');
+			} else if(checkerBoardArray[oneMoveRowRightUp][oneMoveColRightUp] === 'r' && 0 <= twoMovesRowRightUp && 0 <= twoMovesColRight){
+				if(checkerBoardArray[twoMovesRowRightUp][twoMovesColRight] === ' '){
+					$(`.play-checker-tile[row=${twoMovesRowRightUp}][col=${twoMovesColRight}]`).addClass('highlight');
+				}
+			}
+		}		 
+	} else {
+		return;
+	}	
+}
+		
+
+
 function switchPlayer(){
-	debugger;
+	
 	if(blackTurn){
 		blackTurn = false;
 		redTurn = true;
